f70fee87bfb8e015a56bdd0d1fef63ce13264af8
Ryan Karpeles <ryan@teamodea.com>
2016-03-28 00:09:34 -0500

    Update styles

--- wp-content/themes/bulan-child/functions.php
+++ wp-content/themes/bulan-child/functions.php
@@ -1,4 +1,8 @@
 <?php
+
+/* ================================================================
+   Enqueue Child Theme
+   ================================================================ */
 function theme_enqueue_styles() {
 
     $parent_style = 'parent-style';
@@ -10,3 +14,31 @@ function theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+/* ================================================================
+   // Add new styles to the TinyMCE options
+   ================================================================ */
+
+function my_mce_before_init_insert_formats( $init_array ) {  
+  // Define the style_formats array
+  $style_formats = array(  
+    array(
+      'title' => __( 'Colors', 'wpex' ),
+      'items' => array(
+        array(  
+          'title'     => 'Main Orange',  
+          'inline'    => 'span',  
+          'classes'   => 'orange-main',
+        ),
+      ),
+    ),    
+  );  
+  // Insert the array, JSON ENCODED, into 'style_formats'
+  $init_array['style_formats'] = json_encode( $style_formats );  
+  
+  return $init_array;  
+  
+} 
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
